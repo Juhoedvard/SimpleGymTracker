e3ad537e04331be00b426f25249c16cfc3eea95e
Juhoedvard <j.puumalainen95@gmail.com>
2023-11-22 18:41:59 +0200

    Updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: ["/UserPage/:path*", "/auth-callback", "/Charts"],
 };
 
 export default authMiddleware
